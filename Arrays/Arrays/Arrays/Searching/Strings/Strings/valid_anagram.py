8da1e7c93a4e4c2ee51253875b07c273c25f8d53
thungalikitha023-dev <thungalikitha023@gmail.com>
2026-07-31 18:29:55 +0530

    Add Valid Anagram solution

---
+++ Arrays/Arrays/Arrays/Searching/Strings/Strings/valid_anagram.py
@@ -0,0 +1,25 @@
+def is_anagram(s, t):
+    if len(s) != len(t):
+        return False
+
+    count = {}
+
+    for char in s:
+        count[char] = count.get(char, 0) + 1
+
+    for char in t:
+        if char not in count:
+            return False
+
+        count[char] -= 1
+
+        if count[char] < 0:
+            return False
+
+    return True
+
+
+s = "listen"
+t = "silent"
+
+print(is_anagram(s, t))
